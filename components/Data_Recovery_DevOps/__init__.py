c2b7a34ff2c3026731cb427688c074c187a82e95
Patrick Cuba <patrick.cuba@snowflake.com>
2026-04-08 15:05:49 +1000

    Fix missing comp_declarative_pipeline export in Data_Recovery_DevOps __init__
    .... Generated with [Cortex Code](https://docs.snowflake.com/en/user-guide/cortex-code/cortex-code)
    
    Co-Authored-By: Cortex Code <noreply@snowflake.com>

--- components/Data_Recovery_DevOps/__init__.py
+++ components/Data_Recovery_DevOps/__init__.py
@@ -3,6 +3,7 @@ from .recovery_devops_analyzer import comp_recovery_devops_analyzer
 from .dcm_adoption import comp_dcm_adoption
 from .git_integration import comp_git_integration
 from .cicd_automation import comp_cicd_automation
+from .declarative_pipeline import comp_declarative_pipeline
 from .orchestration_patterns import comp_orchestration_patterns
 from .devops_maturity_summary import comp_devops_maturity_summary
 
@@ -12,6 +13,7 @@ __all__ = [
     'comp_dcm_adoption',
     'comp_git_integration',
     'comp_cicd_automation',
+    'comp_declarative_pipeline',
     'comp_orchestration_patterns',
     'comp_devops_maturity_summary'
 ]
